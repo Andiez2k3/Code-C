f502d01322346cdcc8d932cb6fb196f8c002ddde
Andiez2k3 <quyen0946325154@gmail.com>
2026-04-23 08:29:41 +0700

    2

--- chuongtrinh1.cpp
+++ chuongtrinh1.cpp
@@ -4,15 +4,15 @@ int main() {
 wcout << "Xin Chao \n";
     double a,b,c,d;
     double cong,tru,chia,nhan;
-    cout << "\nNhap vao 4 so thuc: ";
+    cout << "Nhap vao 4 so thuc: ";
     cin >> a >> b >> c >> d;
     cong = a + b + c + d;
     tru = a - b - c - d;
     chia = a / b / c / d;
     nhan = a * b * c * d;
-    cout << "Tong: " << cong << endl;
-    cout << "Hieu: " << tru << endl;
-    cout << "Thuong: " << chia << endl;
-    cout << "Tich: " << nhan << endl;
+    cout << "Tong: " << cong ;
+    cout << "Hieu: " << tru ;
+    cout << "Thuong: " << chia;
+    cout << "Tich: " << nhan;
 }
 
